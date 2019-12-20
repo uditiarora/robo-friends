997fe8b9aed4c183056bb50a06437bdd7d85f372
Uditi <uditiarora@gmail.com>
2019-12-20 17:00:06 +0530

    Updates

---
+++ precache-manifest.965d0266f8b9bbf68f54917c5ac4a3a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8075b1ac90007d0c1d330a36d20e079",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "59ba6c43c555c6e28d34",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dbfeb188b92b459a13f9",
+    "url": "/RoboFriends/static/css/main.76dc22f0.chunk.css"
+  },
+  {
+    "revision": "59ba6c43c555c6e28d34",
+    "url": "/RoboFriends/static/js/2.fc5a1ba2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RoboFriends/static/js/2.fc5a1ba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbfeb188b92b459a13f9",
+    "url": "/RoboFriends/static/js/main.c590f548.chunk.js"
+  },
+  {
+    "revision": "96c933f2cfbd169da615",
+    "url": "/RoboFriends/static/js/runtime-main.4a5a56ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
